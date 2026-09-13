7e8c700cbc488333d647dd4499fab179025b6049
MAYKEY ILUS JEAN BAPTISTE <2752838@collgelacite.ca>
2026-09-13 18:41:18 -0400

    connecter frontend aux contenus

--- Bensky website/structure.js
+++ Bensky website/structure.js
@@ -1,88 +1,96 @@
-
-
-/// fonction pour clicke sou element yo 
+/// fonction pour cliquer sur les éléments
 function cli(){
 
-   // variable qui vas pacourir le tableau 
     let items = document.getElementsByClassName("item");
 
-    // boucle qui va pacourir tous tablau a et afficher les page correspondre  
     for (let i = 0; i < items.length; i++) {
-    items[i].addEventListener("click", function() {
 
-        if (i === 0) {
-            window.location.href = "apropos.html";
-        }
+        items[i].addEventListener("click", function() {
 
-        else if (i === 1) {
-            window.location.href = "CONTAK.html";
-        }
+            if (i === 0) {
+                window.location.href = "apropos.html";
+            }
 
-        else if (i === 2) {
-            window.location.href = "sinema.html";
-        }
+            else if (i === 1) {
+                window.location.href = "CONTAK.html";
+            }
 
-        else if (i === 3) {
-            window.location.href = "divesite.html";
-        }
+            else if (i === 2) {
+                window.location.href = "sinema.html";
+            }
 
-        else if (i === 4) {
-            window.location.href = "mizik.html";
-        }
+            else if (i === 3) {
+                window.location.href = "divesite.html";
+            }
 
-        else if (i === 5) {
-            window.location.href = "sport.html";
-        }
+            else if (i === 4) {
+                window.location.href = "mizik.html";
+            }
 
-        else if (i === 6) {
-            window.location.href = "lidechip.html";
-        }
+            else if (i === 5) {
+                window.location.href = "sport.html";
+            }
 
-    });
+            else if (i === 6) {
+                window.location.href = "lidechip.html";
+            }
 
-    
-}
+        });
 
-let pages = [
-    "sinema.html",     // 0
-    "divesite.html",  // 1
-    "lidechip.html", // 2
-    "mizik.html",      // 3
-    "sport.html"       // 4
-];
+    }
 
-let item = document.getElementsByClassName("lebal");
+    let pages = [
+        "sinema.html",
+        "divesite.html",
+        "lidechip.html",
+        "mizik.html",
+        "sport.html"
+    ];
 
-for (let i = 0; i < item.length; i++) {
-    item[i].addEventListener("click", function() {
-        window.location.href = pages[i];
-    });
-}
+    let item = document.getElementsByClassName("lebal");
 
+    for (let i = 0; i < item.length; i++) {
 
+        item[i].addEventListener("click", function() {
+            window.location.href = pages[i];
+        });
 
+    }
 }
 
 cli();
 
 
-
-
-
-
-/// se pou menu troi bar yo
+/// menu trois barres
 const btn = document.getElementById("menu-btn");
 const menu = document.getElementById("menu");
 
 btn.addEventListener("click", function() {
+
     if (menu.style.left === "0px") {
-        menu.style.left = "-200px"; // fèmen
+        menu.style.left = "-200px";
     } else {
-        menu.style.left = "0px"; // ouvri
+        menu.style.left = "0px";
     }
+
 });
 
 
+/// récupérer les articles depuis MySQL via le backend
+fetch("/contenus")
 
-   
- 
+    .then(function(response) {
+        return response.json();
+    })
+
+    .then(function(contenus) {
+
+        console.log("Articles reçus :", contenus);
+
+    })
+
+    .catch(function(error) {
+
+        console.error("Erreur :", error);
+
+    });
